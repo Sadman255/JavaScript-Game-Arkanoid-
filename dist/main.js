6f6f29b7817e94c5d9c0a977d4cf3a3de2cc37ec
Sadman Anwar <sadmanowar@gmail.com>
2020-02-05 12:07:49 -0500

    paddle moving

---
+++ dist/main.js
@@ -0,0 +1,125 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _paddle_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./paddle.js */ \"./src/paddle.js\");\n/* harmony import */ var _input_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./input.js */ \"./src/input.js\");\nconsole.log(\"Webpack is working!\")\n\n\n\n\nlet canvas = document.getElementById(\"arkanoid\");\nlet ctx = canvas.getContext(\"2d\");\n\nconst GAME_WIDTH = 800;\nconst GAME_HEIGHT = 600;\n\nctx.clearRect(0,0,800,600)\n\nlet paddle = new _paddle_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"](GAME_WIDTH, GAME_HEIGHT);\n\nnew _input_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"](paddle);\n\nlet lastTime = 0;\n\nfunction gameLoop(timestamp) {\n    let deltaTime = timestamp - lastTime;\n    lastTime = timestamp;\n    ctx.clearRect(0, 0, 800, 600)\n    paddle.update(deltaTime);\n    paddle.draw(ctx);\n\n    requestAnimationFrame(gameLoop);\n}\n\ngameLoop();\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/input.js":
+/*!**********************!*\
+  !*** ./src/input.js ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return InputHandler; });\n/* harmony import */ var _paddle__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./paddle */ \"./src/paddle.js\");\n\n\nclass InputHandler {\n\n    constructor(paddle){\n        document.addEventListener('keydown', (event) =>{\n          \n\n            switch(event.keyCode){\n                case 37: \n                    paddle.moveLeft();\n                    break;\n                \n                case 39: \n                    paddle.moveRight();\n                    break;\n            }\n        })\n\n        document.addEventListener('keyup', (event) => {\n\n\n            switch (event.keyCode) {\n                case 37:\n                    paddle.stop();\n                    break;\n\n                case 39:\n                    paddle.stop();\n                    break;\n            }\n        })\n    }\n}\n\n//# sourceURL=webpack:///./src/input.js?");
+
+/***/ }),
+
+/***/ "./src/paddle.js":
+/*!***********************!*\
+  !*** ./src/paddle.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Paddle; });\n\n\n class Paddle{\n\n    constructor(gameWidth, gameHeight){\n        this.gameWidth = gameWidth;\n\n        this.width = 150;\n        this.height = 20; //20 instead of 30\n\n        this.maxSpeed = 7;\n        this.speed = 0;\n\n        this.position ={\n            x: gameWidth / 2 - this.width / 2,\n            y: gameHeight - this.height -10\n        }\n    }\n\n    moveLeft(){\n        console.log(\"move left please\")\n        this.speed = -this.maxSpeed;\n    }\n\n    moveRight(){\n        this.speed = this.maxSpeed;\n    }\n\n    stop(){\n        this.speed = 0;\n    }\n\n    draw(ctx){\n        ctx.fillStyle = 'red'\n        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)\n        ctx.strokeStyle = 'yellow'\n        ctx.strokeRect(this.position.x, this.position.y, this.width, this.height)\n        ctx.lineWidth = 4;\n        \n    }\n\n    update(deltaTime){\n        if(!deltaTime) return;\n         this.position.x += this.speed;\n\n        if(this.position.x < 0) this.position.x = 0\n        if(this.position.x + this.width > this.gameWidth){\n            this.position.x = this.gameWidth - this.width;\n        }\n    }\n\n   \n\n   \n\n}\n\n \n\n//# sourceURL=webpack:///./src/paddle.js?");
+
+/***/ })
+
+/******/ });
---
+++ src/paddle.js
@@ -0,0 +1,58 @@
+
+
+ export default class Paddle{
+
+    constructor(gameWidth, gameHeight){
+        this.gameWidth = gameWidth;
+
+        this.width = 150;
+        this.height = 20; //20 instead of 30
+
+        this.maxSpeed = 7;
+        this.speed = 0;
+
+        this.position ={
+            x: gameWidth / 2 - this.width / 2,
+            y: gameHeight - this.height -10
+        }
+    }
+
+    moveLeft(){
+        console.log("move left please")
+        this.speed = -this.maxSpeed;
+    }
+
+    moveRight(){
+        this.speed = this.maxSpeed;
+    }
+
+    stop(){
+        this.speed = 0;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        ctx.strokeStyle = 'yellow'
+        ctx.strokeRect(this.position.x, this.position.y, this.width, this.height)
+        ctx.lineWidth = 4;
+        
+    }
+
+    update(deltaTime){
+        if(!deltaTime) return;
+         this.position.x += this.speed;
+
+        if(this.position.x < 0) this.position.x = 0
+        if(this.position.x + this.width > this.gameWidth){
+            this.position.x = this.gameWidth - this.width;
+        }
+    }
+
+   
+
+   
+
+}
+
+ 
